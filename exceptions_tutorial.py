f1de7483d619d89dc4a036d429e4b4a669d4845e
RAO YANG <lightrao@hotmail.com>
2023-06-25 08:08:33 +0800

    Exceptions

---
+++ exceptions_tutorial.py
@@ -0,0 +1,15 @@
+# Exception: Description, Traceback
+# for i in range(5)
+#     print("Hello, world!")
+
+# print(1 / 0)
+
+# with open("./x_file.txt") as xf:
+#     the_truth = xf.read()
+# print(the_truth)
+
+# print(1 + 2 + "three")
+
+import math  # cmath module
+
+print(math.sqrt(-1))
