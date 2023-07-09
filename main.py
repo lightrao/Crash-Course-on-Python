52a63d16b4b3d9de89267d3406586c02106cbeca
RAO YANG <lightrao@hotmail.com>
2023-07-09 13:34:07 +0800

    about python package

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 # main.py
-import person
+# import person
 
-print(person.weight)  # Alice
-print(person.NUMBER)  # Hello
-print(person.greet("Bob"))  # Hello, Bob!
-print(person.alice.name)  # Alice
-print(person.alice.introduce())  # Hello, Alice!
+# print(person.weight)  # Alice
+# print(person.NUMBER)  # Hello
+# print(person.greet("Bob"))  # Hello, Bob!
+# print(person.alice.name)  # Alice
+# print(person.alice.introduce())  # Hello, Alice!
 
-sam = person.Person("sam")
-print(sam.name)
-print(sam.introduce())
+# sam = person.Person("sam")
+# print(sam.name)
+# print(sam.introduce())
+
+# A Python package is a collection of modules that are organized in a hierarchical structure.
+# A package can contain subpackages, modules, and resources such as data files and images.
+from myutils import common
+from myutils.sub1 import mod1
+from myutils.sub2 import mod2
+
+print(common.add(1, 2))
+mod1.hello()
+mod2.goodbye()
