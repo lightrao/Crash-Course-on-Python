8042ee027d56f1a7092f27841c6089f35bb69636
RAO YANG <lightrao@hotmail.com>
2023-07-09 12:36:41 +0800

    about python module

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
 # main.py
-# -----------------------------------------------------------------------------------------------
-# module: python file containing function and variable that can be imported and used in other file
-import greet
+import person
 
-greet.hello("Neo")
-print(greet.age)
+print(person.weight)  # Alice
+print(person.NUMBER)  # Hello
+print(person.greet("Bob"))  # Hello, Bob!
+print(person.alice.name)  # Alice
+print(person.alice.introduce())  # Hello, Alice!
 
-# The dir() function in Python returns a list of names defined by an object.
-# If the object is a module, it returns the names of the variables, functions, classes, etc. defined in the module.
-# If the object is a class or an instance of a class, it returns the names of the attributes and methods of the class or instance.
-print(dir(greet))
-
-# The dir() function with no parameter returns the list of names in the current local scope.
-# This includes the names of the variables, functions, modules, etc. that are defined or imported
-# in the current scope.
-print(dir())
-
-# -----------------------------------------------------------------------------------------------
-# import the modules directly
-from mypackage import hello, goodbye
-
-# use the functions from the modules
-hello.say_hello("Charlie")
-goodbye.say_goodbye("David")
-
-# -----------------------------------------------------------------------------------------------
-# import the common module directly
-import myutils.common
-
-print(myutils.common.add(1, 2))
-print(myutils.common.sub(1, 2))
-print(myutils.common.mul(1, 2))
-print(myutils.common.div(1, 2))
+sam = person.Person("sam")
+print(sam.name)
+print(sam.introduce())
---
+++ person.py
@@ -0,0 +1,25 @@
+# A Python module is a file containing variables, classes, and other code elements.
+
+# A variable
+weight = 78
+
+# A constant
+NUMBER = "007"
+
+
+# A function
+def greet(name):
+    return f"Hello, {name}."
+
+
+# A class
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def introduce(self):
+        return f"Hello, my name is {self.name}."
+
+
+# An object
+alice = Person("alice")
