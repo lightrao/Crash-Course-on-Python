fabf1a6c362dd65c8c885a8f3c4548b15eb3fd83
RAO YANG <lightrao@hotmail.com>
2023-07-18 07:43:03 +0800

    update

--- python-playground.py
+++ python-playground.py
@@ -2,15 +2,21 @@ import logging
 import math
 
 # create and configure logger
+# level, the name of logger(default is root), the time when the log message is created, message
+# python log format: https://docs.python.org/3/library/logging.html
 LOG_FORMAT = "%(levelname)s:%(name)s:%(asctime)s:%(message)s"
-logging.basicConfig(filename="./lumberjack.log", level=logging.NOTSET, format=LOG_FORMAT, filemode='w')
+
+logging.basicConfig(
+    filename="./lumberjack.log", level=logging.NOTSET, format=LOG_FORMAT, filemode="w"
+)
 logger = logging.getLogger()
 
 # Test the logger
-# print(logger.level)
-# logger.info("Our first message.")
+# print(logger.level) # The default level is Warning=30
+# logger.info("Our first message.") # Loggers will only write message with level greater or equal to the set level
 
 # Test messages
+# You can log messages of any level by calling the method with level name.
 # print(logger.level)
 # logger.debug("This is a harmless debug message.")
 # logger.info("Just some useful info.")
@@ -18,27 +24,28 @@ logger = logging.getLogger()
 # logger.error("An error has occurred, software was unable to perform some function.")
 # logger.critical("A serious error has occurred, program itself may shut down.")
 
-def quadratic_formula(a,b,c):
+
+# Quadratic Formula
+def quadratic_formula(a, b, c):
     """Return the solutions to the equation ax^2 + bx + c = 0"""
-    logger.info(f"quadratic_fomula({a},{b},{c})")
+    logger.info(f" quadratic_fomula({a},{b},{c})")
 
     # Compute the discriminant
     logger.debug("# Compute the discriminant")
-    disc=b**2-4*a*c
+    disc = b**2 - 4 * a * c
 
     # Compute the two roots
     logger.debug("# Compute the two roots")
-    root1=(-b+math.sqrt(disc))/(2*a)
-    root2=(-b-math.sqrt(disc))/(2*a)
+    root1 = (-b + math.sqrt(disc)) / (2 * a)
+    root2 = (-b - math.sqrt(disc)) / (2 * a)
 
     # Return the two roots
     logger.debug("# Return the roots")
     return (root1, root2)
 
-roots=quadratic_formula(1,0,1)
+
+roots = quadratic_formula(1, 0, 1)
 print(roots)
 # roots2=quadratic_formula(1,0,-4)
 # print(roots2)
 # print(math.sqrt(-4))
-
-
